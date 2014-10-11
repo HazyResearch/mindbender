f246d26698311fd3ff49cb5121ca65f33ad5f6a7
Jaeho Shin <netj@cs.stanford.edu>
2014-10-11 00:34:10 -0700

    nodejs build dep fix

--- .depends/nodejs.test.sh
+++ .depends/nodejs.test.sh
@@ -1,9 +1,1 @@
-#!/usr/bin/env bash
-
-# check nodejs version
-version=${DEPENDS_ON_NODE_VERSION:-v0.10.26}
-
-if ! type node npm &>/dev/null || [[ $(node --version) < $version ]]; then
-    echo >&2 "nodejs >= $version not found"
-    false
-fi
+../buildkit/depends/common/nodejs.test.sh
