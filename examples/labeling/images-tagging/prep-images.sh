4e26d6b4ec67dc47cc0e488874a0e2b2d4f0f076
Jaeho Shin <netj@cs.stanford.edu>
2015-03-11 17:45:52 -0700

    Added an example MT task for tagging images
    with a script for obtaining a list of images from Flickr with a certain
    tag (defaulting to "cat" of course).

---
+++ examples/labeling/images-tagging/prep-images.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+# A script for preparing a list of images from Flickr
+# Author: Jaeho Shin <netj@cs.stanford.edu>
+# Created: 2015-03-11
+set -eu
+
+tag=${1:-cat}
+curl 'http://api.flickr.com/services/feeds/photos_public.gne?format=json&nojsoncallback=1&tags='"$tag"'' >flickr-${tag}.json
+mindbender hack coffee -e '
+fs = require "fs"
+stdin = fs.readFileSync "/dev/stdin"
+try feed = JSON.parse stdin
+catch err then eval "feed = #{stdin}"
+console.log JSON.stringify feed?.items, null, 1
+' <flickr-${tag}.json >images-${tag}.json
+ln -sfnv images-${tag}.json images.json
