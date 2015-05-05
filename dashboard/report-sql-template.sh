64def0f3af4c890af24cd6ae36673d059dc1bc0d
Jaeho Shin <netj@cs.stanford.edu>
2015-05-04 17:37:53 -0700

    Fixes report-sql-template.sh's permission and header

--- dashboard/report-sql-template.sh
+++ dashboard/report-sql-template.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+# Common report.sh for report.sql.in-based report templates
+# Author: Jaeho Shin <netj@cs.stanford.edu>
+# Created: 2015-04-29
 set -eu
 inputName=report
 outputName=report
