dfe0cc0cef2634c1a419abbf3bb9f8aa075ff5e6
Jaeho Shin <netj@cs.stanford.edu>
2015-05-12 00:13:56 -0700

    Migrates dashboard report templates into new format
    - Also renames report.sql.in to data.sql.in as it collides with report.json

--- dashboard/report-sql-template.sh
+++ dashboard/report-sql-template.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
-# Common report.sh for report.sql.in-based report templates
+# Common report.sh for data.sql.in-based report templates
 # Author: Jaeho Shin <netj@cs.stanford.edu>
 # Created: 2015-04-29
 set -eu
-inputName=report
-outputName=report
+inputName=data
+outputName=data
+EXPAND_PARAMETERS=true \
 compile-xdocs "$inputName".sql.in
 run-sql "$(cat "$inputName".sql)" CSV HEADER >"$outputName".csv
 json-for "$outputName".csv >"$outputName".json
