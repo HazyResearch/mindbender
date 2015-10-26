d3293c647d4be8690788dc0647f69f7856d140d0
Raphael Hoffmann <raphael.hoffmann@gmail.com>
2015-10-26 17:53:11 +0000

    Merges adjacent highlights into a single highlight

--- gui/frontend/src/search/vis/TextWithAnnotations.js
+++ gui/frontend/src/search/vis/TextWithAnnotations.js
@@ -8,10 +8,28 @@ var TextWithAnnotations = (function() {
             .attr({'style':'white-space:pre-wrap'})
             .append(jQuery.parseHTML(content))
 
-    var extractions = obj._source.extractions 
-    for (var i=0; i < extractions.length; i++) {
-       var ex = JSON.parse(extractions[i]);
-       new SpansVisualization(el[0], ex.selections, ex.extractor)
+    var extractions = obj._source.extractions
+    if (typeof extractions !== 'undefined') { 
+      for (var i=0; i < extractions.length; i++) {
+         var ex = JSON.parse(extractions[i]);
+         // see if we should merge multiple selections into a
+         // a single one, we merge if there's only one char in
+         // between and it's part of the same extraction
+         var sels = []
+         for (var j=0; j < ex.selections.length; j++) {
+            if (sels.length > 0) {
+               var prev_end = sels[sels.length-1][1]
+               var next_sta = ex.selections[j][0]
+               if (next_sta - prev_end <= 1) {
+                  sels[sels.length-1][1] = ex.selections[j][1]
+                  continue
+               }
+            }
+            sels.push(ex.selections[j])
+         }
+
+         new SpansVisualization(el[0], sels, ex.extractor)
+      }
     }
     return el
   }
